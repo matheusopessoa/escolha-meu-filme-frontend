b622a22677da7af581c94f4f534a3a31e04e5215
matheus <matheuspessoa.tech@gmail.com>
2024-10-16 18:31:07 -0300

    reset localstorage

--- src/assets/screens/Home.jsx
+++ src/assets/screens/Home.jsx
@@ -1,20 +1,30 @@
-// src/screens/Home.jsx
-import React, { useState } from 'react';
+import React, { useContext } from 'react';
 import Title from '../components/Title';
 import Subtitle from '../components/Subtitle';
 import Button from '../components/Button';
 import Background from '../components/Background';
 import Container from '../components/Container';
 import { useNavigate } from 'react-router-dom';
+import { AppContext } from '../../context'; // Importa o contexto para acessar os setters
 
 const Home = () => {
   const navigate = useNavigate();
-  
+  const { setSelectedService, setSelectedGenres, setMovies, setTheMovie } = useContext(AppContext);
+
   const handleButtonClick = () => {
+    // Limpa o localStorage
+    localStorage.clear();
+
+    // Reseta o contexto
+    setSelectedService(null);
+    setSelectedGenres([]);
+    setMovies({});
+    setTheMovie([]);
+
+    // Navega para a próxima tela
     navigate('/streaming');
   };
 
- 
   return (
     <Background>
       <Container>
