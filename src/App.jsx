f233ea4102dd6e3e46ed998b8cbfa874a4e5c254
matheus <matheuspessoa.tech@gmail.com>
2024-10-16 12:07:08 -0300

    analytics

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Analytics } from "@vercel/analytics/react"
+import { Analytics } from "@vercel/analytics/react";
 import { AppProvider } from './context.jsx'; // O provedor do contexto
 import FirstScreen from './assets/screens/Home.jsx'; // Tela inicial
 
@@ -8,8 +8,8 @@ function App() {
     <AppProvider>
       <div className="App">
         <FirstScreen /> {/* Mantemos a lógica de renderização existente */}
+        <Analytics /> {/* Coloque aqui dentro para monitorar todas as interações */}
       </div>
-      <Analytics />
     </AppProvider>
   );
 }
